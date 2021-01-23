0b727cfebc8a3083a23c3d1919c68173b0551d21
Juan Pablo Garzon <jgarzonebrath08@gmail.com>
2021-01-23 09:45:36 -0500

    Unregistered companies search endpoint changed

--- searches/urls.py
+++ searches/urls.py
@@ -8,7 +8,7 @@ from searches.views import *
 
 router = routers.DefaultRouter()
 router.register('companies', SearchCompaniesViewSet, basename="search__companies")
-router.register('companies/unregistered', SearchUnregisteredCompaniesViewSet, basename="search__unregistered_companies")
+router.register('unregistered-companies', SearchUnregisteredCompaniesViewSet, basename="search__unregistered_companies")
 router.register('products', SearchShowcaseProductsViewSet, basename="search__products")
 
 urlpatterns = [
